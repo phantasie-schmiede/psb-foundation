bd37babe14d164f6078f458ce1d7f0549877e788
Daniel Ablass <dnartists@web.de>
2021-05-19 09:35:00 +0200

    Add composer.json.

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF['psb_foundation'] = [
         ],
         'depends'   => [
             'php'   => '7.4',
-            'typo3' => '10.4.10-10.4.99',
+            'typo3' => '10.4.10-11.2.99',
         ],
         'suggests'  => [
         ],
