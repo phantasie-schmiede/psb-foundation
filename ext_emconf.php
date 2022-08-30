5731274f2a57cbdbfcc1bcc26ddccbbbadc7674f
Daniel Ablass <dnartists@web.de>
2022-08-30 08:29:08 +0200

    [TASK] Add PHP 8 to compatibility information.

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
         'conflicts' => [
         ],
         'depends'   => [
-            'php'   => '7.4',
+            'php'   => '7.4-8.1',
             'typo3' => '11.5.5-11.5.99',
         ],
         'suggests'  => [
