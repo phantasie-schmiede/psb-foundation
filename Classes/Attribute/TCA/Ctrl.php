aa7ae051d66dafb4faf6b02f9edbe7a7472dffee
Daniel Ablass <daniel.ablass@plan.de>
2023-05-19 15:08:24 +0200

    [TASK] Apply CGL to Ctrl property names.

--- Classes/Attribute/TCA/Ctrl.php
+++ Classes/Attribute/TCA/Ctrl.php
@@ -43,14 +43,14 @@ class Ctrl extends AbstractTcaAttribute
      * @param array|null        $container                        https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Container.html
      * @param string|null       $copyAfterDuplFields              https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/CopyAfterDuplFields.html
      * @param string|null       $crdate                           https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Crdate.html
-     * @param string            $cruser_id                        https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/CruserId.html
-     * @param string|null       $default_sortby                   https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/DefaultSortby.html
+     * @param string            $cruserId                         https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/CruserId.html
+     * @param string|null       $defaultSortBy                    https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/DefaultSortby.html
      * @param string|null       $delete                           https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Delete.html
      * @param string|null       $descriptionColumn                https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/DescriptionColumn.html
      * @param string|null       $editlock                         https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Editlock.html
      * @param array|null        $enablecolumns                    https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Enablecolumns.html
-     * @param string|null       $formattedLabel_userFunc          https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/FormattedLabelUserFunc.html
-     * @param array|null        $formattedLabel_userFunc_options  https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/FormattedLabelUserFuncOptions.html
+     * @param string|null       $formattedLabelUserFunc           https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/FormattedLabelUserFunc.html
+     * @param array|null        $formattedLabelUserFuncOptions    https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/FormattedLabelUserFuncOptions.html
      * @param string|null       $groupName                        https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/GroupName.html
      * @param bool|null         $hideAtCopy                       https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/HideAtCopy.html
      * @param bool|null         $hideTable                        https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/HideTable.html
@@ -58,11 +58,11 @@ class Ctrl extends AbstractTcaAttribute
      * @param bool|null         $ignorePageTypeRestriction        https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Security.html
      * @param bool|null         $ignoreRootLevelRestriction       https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Security.html
      * @param bool|null         $ignoreWebMountRestriction        https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Security.html
-     * @param bool|null         $is_static                        https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/IsStatic.html
+     * @param bool|null         $isStatic                         https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/IsStatic.html
      * @param string|null       $label                            https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Label.html
-     * @param array|string|null $label_alt                        https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Label.html
-     * @param bool|null         $label_alt_force                  https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Label.html
-     * @param string|null       $label_userFunc                   https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/LabelUserfunc.html
+     * @param array|string|null $labelAlt                         https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Label.html
+     * @param bool|null         $labelAltForce                    https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Label.html
+     * @param string|null       $labelUserFunc                    https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/LabelUserfunc.html
      * @param string|null       $languageField                    https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/LanguageField.html
      * @param string|null       $origUid                          https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/OrigUid.html
      * @param string|null       $prependAtCopy                    https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/PrependAtCopy.html
@@ -70,7 +70,7 @@ class Ctrl extends AbstractTcaAttribute
      * @param int|null          $rootLevel                        https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/RootLevel.html
      * @param array|null        $searchFields                     https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/SearchFields.html
      * @param array|null        $security                         https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Security.html
-     * @param string|null       $selicon_field                    https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/SeliconField.html
+     * @param string|null       $seliconField                     https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/SeliconField.html
      * @param string|null       $shadowColumnsForNewPlaceholders  https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/ShadowColumnsForNewPlaceholders.html
      * @param string|null       $sortby                           https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Sortby.html
      * @param string|null       $title                            https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Title.html
@@ -79,61 +79,61 @@ class Ctrl extends AbstractTcaAttribute
      * @param string|null       $translationSource                https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/TranslationSource.html
      * @param string|null       $tstamp                           https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Tstamp.html
      * @param string|null       $type                             https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/Type.html
-     * @param array|null        $typeicon_classes                 https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/TypeiconClasses.html
-     * @param string|null       $typeicon_column                  https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/TypeiconColumn.html
+     * @param array|null        $typeiconClasses                  https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/TypeiconClasses.html
+     * @param string|null       $typeiconColumn                   https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/TypeiconColumn.html
      * @param string|null       $useColumnsForDefaultValues       https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/UseColumnsForDefaultValues.html
      * @param bool|null         $versioningWS                     https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/VersioningWS.html
      * @param bool|null         $versioningWS_alwaysAllowLiveEdit https://docs.typo3.org/m/typo3/reference-tca/main/en-us/Ctrl/Properties/VersioningWSAlwaysAllowLiveEdit.html
      */
     public function __construct(
-        protected ?array $EXT = null,
-        protected ?bool $adminOnly = null,
-        protected ?array $container = null,
-        protected ?string $copyAfterDuplFields = null,
-        protected ?string $crdate = 'crdate',
-        protected string $cruser_id = 'cruser_id',
-        protected ?string $default_sortby = 'uid DESC',
-        protected ?string $delete = 'deleted',
-        protected ?string $descriptionColumn = null,
-        protected ?string $editlock = null,
-        protected ?array $enablecolumns = self::ENABLE_COLUMNS,
-        protected ?string $formattedLabel_userFunc = null,
-        protected ?array $formattedLabel_userFunc_options = null,
-        protected ?string $groupName = null,
-        protected ?bool $hideAtCopy = null,
-        protected ?bool $hideTable = null,
-        protected ?string $iconfile = 'EXT:core/Resources/Public/Icons/T3Icons/svgs/mimetypes/mimetypes-x-sys_action.svg',
-        protected ?bool $ignorePageTypeRestriction = null,
-        protected ?bool $ignoreRootLevelRestriction = null,
-        protected ?bool $ignoreWebMountRestriction = null,
-        protected ?bool $is_static = null,
+        protected ?array            $EXT = null,
+        protected ?bool             $adminOnly = null,
+        protected ?array            $container = null,
+        protected ?string           $copyAfterDuplFields = null,
+        protected ?string           $crdate = 'crdate',
+        protected string            $cruserId = 'cruser_id',
+        protected ?string           $defaultSortBy = 'uid DESC',
+        protected ?string           $delete = 'deleted',
+        protected ?string           $descriptionColumn = null,
+        protected ?string           $editlock = null,
+        protected ?array            $enablecolumns = self::ENABLE_COLUMNS,
+        protected ?string           $formattedLabelUserFunc = null,
+        protected ?array            $formattedLabelUserFuncOptions = null,
+        protected ?string           $groupName = null,
+        protected ?bool             $hideAtCopy = null,
+        protected ?bool             $hideTable = null,
+        protected ?string           $iconfile = 'EXT:core/Resources/Public/Icons/T3Icons/svgs/mimetypes/mimetypes-x-sys_action.svg',
+        protected ?bool             $ignorePageTypeRestriction = null,
+        protected ?bool             $ignoreRootLevelRestriction = null,
+        protected ?bool             $ignoreWebMountRestriction = null,
+        protected ?bool             $isStatic = null,
         /** You can use the property name. It will be converted to the column name automatically. */
-        protected ?string $label = 'uid',
+        protected ?string           $label = 'uid',
         /** You can use property names. They will be converted to their column names automatically. */
-        protected array|string|null $label_alt = null,
-        protected ?bool $label_alt_force = null,
-        protected ?string $label_userFunc = null,
-        protected ?string $languageField = 'sys_language_uid',
-        protected ?string $origUid = 't3_origuid',
-        protected ?string $prependAtCopy = null,
-        protected ?bool $readOnly = null,
-        protected ?int $rootLevel = null,
-        protected ?array $searchFields = null,
-        protected ?array $security = null,
-        protected ?string $selicon_field = null,
-        protected ?string $shadowColumnsForNewPlaceholders = null,
-        protected ?string $sortby = null,
-        protected ?string $title = null,
-        protected ?string $transOrigDiffSourceField = 'l10n_diffsource',
-        protected ?string $transOrigPointerField = 'l10n_parent',
-        protected ?string $translationSource = 'l10n_source',
-        protected ?string $tstamp = 'tstamp',
-        protected ?string $type = null,
-        protected ?array $typeicon_classes = null,
-        protected ?string $typeicon_column = null,
-        protected ?string $useColumnsForDefaultValues = null,
-        protected ?bool $versioningWS = null,
-        protected ?bool $versioningWS_alwaysAllowLiveEdit = null,
+        protected array|string|null $labelAlt = null,
+        protected ?bool             $labelAltForce = null,
+        protected ?string           $labelUserFunc = null,
+        protected ?string           $languageField = 'sys_language_uid',
+        protected ?string           $origUid = 't3_origuid',
+        protected ?string           $prependAtCopy = null,
+        protected ?bool             $readOnly = null,
+        protected ?int              $rootLevel = null,
+        protected ?array            $searchFields = null,
+        protected ?array            $security = null,
+        protected ?string           $seliconField = null,
+        protected ?string           $shadowColumnsForNewPlaceholders = null,
+        protected ?string           $sortby = null,
+        protected ?string           $title = null,
+        protected ?string           $transOrigDiffSourceField = 'l10n_diffsource',
+        protected ?string           $transOrigPointerField = 'l10n_parent',
+        protected ?string           $translationSource = 'l10n_source',
+        protected ?string           $tstamp = 'tstamp',
+        protected ?string           $type = null,
+        protected ?array            $typeiconClasses = null,
+        protected ?string           $typeiconColumn = null,
+        protected ?string           $useColumnsForDefaultValues = null,
+        protected ?bool             $versioningWS = null,
+        protected ?bool             $versioningWS_alwaysAllowLiveEdit = null,
     ) {
         parent::__construct();
     }
@@ -175,7 +175,7 @@ class Ctrl extends AbstractTcaAttribute
      */
     public function getCruserId(): string
     {
-        return $this->cruser_id;
+        return $this->cruserId;
     }
 
     /**
@@ -183,7 +183,7 @@ class Ctrl extends AbstractTcaAttribute
      */
     public function getDefaultSortBy(): ?string
     {
-        return $this->default_sortby;
+        return $this->defaultSortBy;
     }
 
     /**
@@ -231,7 +231,7 @@ class Ctrl extends AbstractTcaAttribute
      */
     public function getFormattedLabelUserFunc(): ?string
     {
-        return $this->formattedLabel_userFunc;
+        return $this->formattedLabelUserFunc;
     }
 
     /**
@@ -239,7 +239,7 @@ class Ctrl extends AbstractTcaAttribute
      */
     public function getFormattedLabelUserFuncOptions(): ?array
     {
-        return $this->formattedLabel_userFunc_options;
+        return $this->formattedLabelUserFuncOptions;
     }
 
     /**
@@ -279,7 +279,7 @@ class Ctrl extends AbstractTcaAttribute
      */
     public function getIsStatic(): ?bool
     {
-        return $this->is_static;
+        return $this->isStatic;
     }
 
     /**
@@ -301,17 +301,17 @@ class Ctrl extends AbstractTcaAttribute
      */
     public function getLabelAlt(): ?string
     {
-        if (null === $this->label_alt) {
+        if (null === $this->labelAlt) {
             return null;
         }
 
-        if (is_string($this->label_alt)) {
-            $altLabels = GeneralUtility::trimExplode(',', $this->label_alt);
+        if (is_string($this->labelAlt)) {
+            $altLabels = GeneralUtility::trimExplode(',', $this->labelAlt);
         } else {
-            $altLabels = $this->label_alt;
+            $altLabels = $this->labelAlt;
         }
 
-        array_walk($altLabels, function (&$item) {
+        array_walk($altLabels, function(&$item) {
             $item = $this->tcaService->convertPropertyNameToColumnName($item);
         });
 
@@ -323,7 +323,7 @@ class Ctrl extends AbstractTcaAttribute
      */
     public function getLabelAltForce(): ?bool
     {
-        return $this->label_alt_force;
+        return $this->labelAltForce;
     }
 
     /**
@@ -331,7 +331,7 @@ class Ctrl extends AbstractTcaAttribute
      */
     public function getLabelUserFunc(): ?string
     {
-        return $this->label_userFunc;
+        return $this->labelUserFunc;
     }
 
     /**
@@ -376,6 +376,9 @@ class Ctrl extends AbstractTcaAttribute
 
     /**
      * @return string|null
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     * @throws ReflectionException
      */
     public function getSearchFields(): ?string
     {
@@ -384,7 +387,7 @@ class Ctrl extends AbstractTcaAttribute
         }
 
         $searchFields = $this->searchFields;
-        array_walk($searchFields, function (&$item) {
+        array_walk($searchFields, function(&$item) {
             $item = $this->tcaService->convertPropertyNameToColumnName($item);
         });
 
@@ -416,7 +419,7 @@ class Ctrl extends AbstractTcaAttribute
      */
     public function getSeliconField(): ?string
     {
-        return $this->selicon_field;
+        return $this->seliconField;
     }
 
     /**
@@ -488,19 +491,22 @@ class Ctrl extends AbstractTcaAttribute
      */
     public function getTypeiconClasses(): ?array
     {
-        return $this->typeicon_classes;
+        return $this->typeiconClasses;
     }
 
     /**
      * @return string|null
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     * @throws ReflectionException
      */
     public function getTypeiconColumn(): ?string
     {
-        if (null === $this->typeicon_column) {
+        if (null === $this->typeiconColumn) {
             return null;
         }
 
-        return $this->tcaService->convertPropertyNameToColumnName($this->typeicon_column);
+        return $this->tcaService->convertPropertyNameToColumnName($this->typeiconColumn);
     }
 
     /**
