1f81c206584b158e9db8c9c8af6b66f9e71b5e46
Daniel Ablass <daniel.ablass@plan.de>
2023-05-19 09:20:29 +0200

    [TASK] Fix missing upperCamelCase spelling of properties.

--- Classes/Attribute/TCA/ColumnType/File.php
+++ Classes/Attribute/TCA/ColumnType/File.php
@@ -24,8 +24,8 @@ class File extends AbstractColumnType
 
     /**
      * @param array|string $allowed                             https://docs.typo3.org/m/typo3/reference-tca/main/en-us/ColumnsConfig/Type/File/Properties/Allowed.html
-     * @param int|null     $maxitems                            https://docs.typo3.org/m/typo3/reference-tca/main/en-us/ColumnsConfig/Type/File/Properties/Maxitems.html
-     * @param int|null     $minitems                            https://docs.typo3.org/m/typo3/reference-tca/main/en-us/ColumnsConfig/Type/File/Properties/Minitems.html
+     * @param int|null     $maxItems                            https://docs.typo3.org/m/typo3/reference-tca/main/en-us/ColumnsConfig/Type/File/Properties/Maxitems.html
+     * @param int|null     $minItems                            https://docs.typo3.org/m/typo3/reference-tca/main/en-us/ColumnsConfig/Type/File/Properties/Minitems.html
      * @param array|null   $overrideChildTca                    https://docs.typo3.org/m/typo3/reference-tca/main/en-us/ColumnsConfig/Type/File/Properties/OverrideChildTCa.html
      * @param array|null   $upload
      * @param bool         $uploadFileNameGeneratorAppendHash
@@ -40,8 +40,8 @@ class File extends AbstractColumnType
      */
     public function __construct(
         protected array|string $allowed = 'common-image-types',
-        protected ?int         $maxitems = null,
-        protected ?int         $minitems = null,
+        protected ?int         $maxItems = null,
+        protected ?int         $minItems = null,
         protected ?array       $overrideChildTca = null,
         protected ?array       $upload = null,
         protected bool         $uploadFileNameGeneratorAppendHash = true,
@@ -65,17 +65,17 @@ class File extends AbstractColumnType
     /**
      * @return int|null
      */
-    public function getMaxitems(): ?int
+    public function getMaxItems(): ?int
     {
-        return $this->maxitems;
+        return $this->maxItems;
     }
 
     /**
      * @return int|null
      */
-    public function getMinitems(): ?int
+    public function getMinItems(): ?int
     {
-        return $this->minitems;
+        return $this->minItems;
     }
 
     /**
