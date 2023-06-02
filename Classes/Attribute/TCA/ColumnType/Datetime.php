869e3c324433cc68f823e7b702423fc79401e38c
Daniel Ablass <daniel.ablass@plan.de>
2023-06-02 08:37:56 +0200

    [BUGFIX] Fix wrong return type.

--- Classes/Attribute/TCA/ColumnType/Datetime.php
+++ Classes/Attribute/TCA/ColumnType/Datetime.php
@@ -52,9 +52,9 @@ class Datetime extends AbstractColumnType
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getDbType(): string
+    public function getDbType(): ?string
     {
         return $this->dbType?->value;
     }
