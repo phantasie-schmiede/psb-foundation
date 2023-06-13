82ce49cc0e97dcbcd0f3646d3b9d7267408a1785
Daniel Ablass <daniel.ablass@plan.de>
2023-06-13 08:21:23 +0200

    [TASK] Remove unneccesary display details for integer database fields.

--- Classes/Attribute/TCA/ColumnType/AbstractColumnType.php
+++ Classes/Attribute/TCA/ColumnType/AbstractColumnType.php
@@ -23,8 +23,8 @@ abstract class AbstractColumnType extends AbstractAttribute implements ColumnTyp
     public const DATABASE_DEFINITIONS = [
         'BITMAP_32'        => 'tinyint unsigned DEFAULT \'0\'',
         'DECIMAL'          => 'double(11,2) DEFAULT \'0.00\'',
-        'INTEGER_SIGNED'   => 'int(11) DEFAULT \'0\'',
-        'INTEGER_UNSIGNED' => 'int(11) unsigned DEFAULT \'0\'',
+        'INTEGER_SIGNED'   => 'int DEFAULT \'0\'',
+        'INTEGER_UNSIGNED' => 'int unsigned DEFAULT \'0\'',
         'STRING'           => 'varchar(255) DEFAULT \'\'',
         'TEXT'             => 'text',
     ];
